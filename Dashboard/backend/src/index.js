1d8d2a09cb80c2955b690b8b08809e86fc3924f3
Jerry <jerryvas21@gmail.com>
2026-06-13 13:37:55 -0400

    connected frontend to backend

--- Dashboard/backend/src/index.js
+++ Dashboard/backend/src/index.js
@@ -14,6 +14,10 @@ app.use('/api', AnimalsRouter);
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Grazioso Salvare Animal Shelter API!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
